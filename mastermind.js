e08cb6f1680896b6c72b74d814c17b1a073b8c77
Mhd Alaa Darkashli <99034433@mydavinci.nl>
2017-03-15 11:04:46 +0100

    The first step

---
+++ mastermind.js
@@ -0,0 +1,26 @@
+function myFunction(){
+	var Choosen = document.getElementById('mySelect');
+}
+
+
+
+
+
+/*var secretCode = ['red', 'blue', 'green', 'blue'];
+var input = ['red', 'green', 'yellow', 'blue'];
+
+function compare(code1, code2){
+	for(var i = 0; i < code1.length; i++){
+		//code
+		if(code1[i] === code2[i]){
+			console.log("correct!!!");
+		}
+		else{
+			console.log("Miss!!!");
+		}
+
+
+	}
+}
+
+compare(secretCode, input);*/
